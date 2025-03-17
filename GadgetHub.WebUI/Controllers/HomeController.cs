c5bc81da41918787889194a45f51b8b5de223a9c
Amelia Marrone <68938110+ameliamaedae@users.noreply.github.com>
2025-03-17 10:47:12 -0500

    Problem with data fetching

--- GadgetHub.WebUI/Controllers/HomeController.cs
+++ GadgetHub.WebUI/Controllers/HomeController.cs
@@ -1,31 +1,47 @@
 using System.Diagnostics;
+using GadgetHub.Domain.Data;
 using GadgetHub.WebUI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace GadgetHub.WebUI.Controllers;
-
-public class HomeController : Controller
+namespace GadgetHub.WebUI.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly GadgetHubContext _context;
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(GadgetHubContext context, ILogger<HomeController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products.Include(p => p.Category).ToListAsync();
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var productViewModels = products.Select(p => new ProductViewModel
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Description = p.Description,
+                PriceFormatted = p.Price.ToString("C"),
+                CategoryName = p.Category?.Name ?? "Uncategorized"
+            }).ToList();
+
+            return View(productViewModels);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
