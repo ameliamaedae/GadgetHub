c61ed62a7c8c11bafd87b853f764e6afddc05c05
amelia <mae@maedae.io>
2025-04-11 17:52:20 -0500

    db and entity

--- GadgetHub.Domain/Data/DbContext.cs
+++ GadgetHub.Domain/Data/DbContext.cs
@@ -13,13 +13,17 @@ public class GadgetHubContext : DbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
 
-    // Optionally, override OnModelCreating to configure relationships and constraints
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        // For example, configure precision for Price:
+
         modelBuilder.Entity<Product>()
             .Property(p => p.Price)
             .HasColumnType("decimal(18,2)");
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Id)
+            .ValueGeneratedOnAdd();
     }
 }
--- GadgetHub.Domain/Entities/Product.cs
+++ GadgetHub.Domain/Entities/Product.cs
@@ -1,9 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GadgetHub.Domain.Entities;
 
 public class Product
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     [Required] [StringLength(60)] public string Name { get; set; } = string.Empty;
